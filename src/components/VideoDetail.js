9729d7288a7f6e86ab50893b5114ac9c8ebd3f9d
Desktopy12 <Keeley.lp@gmail.com>
2021-04-25 18:38:17 +0100

    application now shows the video onclick within an iframe

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,12 +5,19 @@ const VideoDetail = ({ video }) => {
     return <div>Loading...</div>;
   }
 
-  return <div>
-      <div className="ui segment">
-      <h4 className="ui header">{video.snippet.title}</h4>
-      <p>{video.snippet.description}</p>
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} />
       </div>
-      </div>;
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
