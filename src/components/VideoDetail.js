bb983b3b15f581de22c64b23d929dc97becff6e5
Desktopy12 <Keeley.lp@gmail.com>
2021-04-25 20:15:21 +0100

    Added default video to display react videos and any search now displays the first video in the 5 query limit

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoDetail = ({ video }) => {
   if (!video) {
-    return <div>Loading...</div>;
+    return <div>JUST LOADING IF YOU CAN READ THIS TEXT THEN YOUR INTERNET IS SLOW</div>;
   }
 
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
